076049e65a53b8645f42fb1f2b3ee04a354e3f2a
M1naR0blox <144395767+M1naR0blox@users.noreply.github.com>
2023-11-05 19:02:03 -0600

    delete some bad things

--- games.py
+++ games.py
@@ -8,31 +8,20 @@ def random_num(arg2, arg3):
 def text(arg):
     print(arg)
 
-def games(option):
-    if option == "":
-        text("To choose a game, please put in on the code")
-
-    if option == "help":
-        text("Type the name, not the number")
-        text("1.- Guess the number")
-        text("2.- Coming soon")
-
-    if option == "Guess the number":
-        randomnumber = random_num(1, 100000)
-        text("Guess the number")
-        while True:
-            playernumber = int(input(">>> "))
-            if playernumber > randomnumber:
-                text("You Passed It")
-            if playernumber < randomnumber:
-                text("You Havent Passed It")
-            if playernumber == randomnumber:
-                text("You Won!")
-            if playernumber == "break":
-                break
+def guess_the_num():
+    randomnumber = random_num(1, 100000)
+    text("Guess the number")
+    while True:
+        playernumber = int(input(">>> "))
+        if playernumber > randomnumber:
+            text("You Passed It")
+        if playernumber < randomnumber:
+            text("You Havent Passed It")
+        if playernumber == randomnumber:
+            text("You Won!")
+        if playernumber == "break":
+            break
 
 
     else:
         text("This game aint existing")
-
-games("Guess the number")
